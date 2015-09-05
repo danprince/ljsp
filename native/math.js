0aa7857bf4194b6e01e9316b661dd341f05b8534
danprince <danielprince@hotmail.co.uk>
2015-09-05 14:05:41 +0100

    add equality function

--- native/math.js
+++ native/math.js
@@ -1,6 +1,14 @@
 var util = require('../util');
 
 var math = {
+  '=': function() {
+    var terms = util.toArray(arguments),
+        p = terms[0];
+
+    return terms.reduce(function(equal, q) {
+      return equal && (q === p);
+    }, true);
+  },
   '+': function() {
     var ns = util.toArray(arguments);
     return ns.reduce(function(a, b) {
